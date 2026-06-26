621fa4e3310a15056b493725341cffc3039ce79c
bora <divyanshi.bora@kredmint.com>
2026-06-26 12:35:35 +0530

    Initial commit

---
+++ src/main/java/com/kredmint/loom/LoomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kredmint.loom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoomServiceApplication.class, args);
+	}
+
+}
